22a8b6c6c99f2aedcb76a37f316ae0f4f40ad7eb
Meow <tabbathapischke@gmail.com>
2023-11-28 11:37:57 -1000

    fix bug in product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -40,36 +40,16 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+router.post('/', async (req, res) => {
+  // create a new product
+  try {
+    const productData = await Product.create(req.body);
+    //run 200 status code and provide productData if the request is successful
+    res.status(200).json(productData);
+  } catch (error) {
+    // db error if not successful
+    res.status(500).json(error);
+  }
 });
 
 // update product
